ec74e10b5e6dc7e1d1a1f039e5d34fe859b2ce62
Snakeye <overflow2@snakeye.us>
2015-02-01 10:19:39 +0000

    Update MasterCaution.py

--- Master-Caution/MasterCaution.py
+++ Master-Caution/MasterCaution.py
@@ -9,12 +9,12 @@ pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.init()
 
 soundA = pygame.mixer.Sound("/home/pi/Space/CWS.wav") 
-soundB = pygame.mixer.Sound("/home/pi/Space/JFS.wav")
-soundC = pygame.mixer.Sound("/home/pi/Space/PU.wav")
+soundB = pygame.mixer.Sound("/home/pi/Space/JFS.wav")  # SoundB and C unused; this code was borrowed from a 
+soundC = pygame.mixer.Sound("/home/pi/Space/PU.wav")   # soundboard example and I was too lazy to take it out.
 
 soundChannelA = pygame.mixer.Channel(1) 
-soundChannelB = pygame.mixer.Channel(2)
-soundChannelC = pygame.mixer.Channel(3)
+soundChannelB = pygame.mixer.Channel(2)     # SoundB and C unused; this code was borrowed from a 
+soundChannelC = pygame.mixer.Channel(3)     # soundboard example and I was too lazy to take it out.
 
 print "Sampler Ready."
 Cockpit = serial.Serial("/dev/ttyACM0",115200)
@@ -29,8 +29,8 @@ while True:
 	    Cockpit.write('H')
         if (val == 4):
             soundA.stop()
-        if (val == 3):
-            soundChannelC.play(soundC)
+        if (val == 3):                     # SoundB and C unused; this code was borrowed from a    
+            soundChannelC.play(soundC)     # soundboard example and I was too lazy to take it out.
         val = 0
         sleep(.01)
     except KeyboardInterrupt: 
